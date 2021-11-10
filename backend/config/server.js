ee7437f838d1e3d79af63909c3d8549ff72eb127
Daniel Muxel <daniel.muxel@advery.ch>
2021-11-10 15:54:22 +0100

    initial project created

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '3ee9bc23861880cc8e9f30b9117665e3'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'f08a7c40-fe35-4645-b325-478b8c6077bd'
+};
